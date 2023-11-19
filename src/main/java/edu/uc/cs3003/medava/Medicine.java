09522f2ca8a90050d02d6a969fc85523ad0f3d82
Jack51003 <116380128+Jack51003@users.noreply.github.com>
2023-11-19 19:04:37 +0000

    submission

--- src/main/java/edu/uc/cs3003/medava/Medicine.java
+++ src/main/java/edu/uc/cs3003/medava/Medicine.java
@@ -1,5 +1,33 @@
 package edu.uc.cs3003.medava;
 
-public class Medicine {
+public abstract class Medicine implements Shippable {
+    // Instance variable
+    private String mMedicineName;
 
+    // Getter
+    public String getMedicineName() {
+        return mMedicineName;
+    }
+
+    public double minimumTemperature() {
+        return 0.0;
+    }
+
+    public double maximumTemperature() {
+        return 100.0;
+    }
+
+    public boolean isTemperatureRangeAcceptable(Double lowTemperature, Double highTemperature) {
+        if (this.minimumTemperature() <= lowTemperature && highTemperature <= this.maximumTemperature()) {
+            return true;
+        }
+        return false;
+    }
+
+    // Constructor
+    public Medicine(String medicineName) {
+        mMedicineName = medicineName;
+    }
+
+    public abstract MedicineSchedule getSchedule();
 }
